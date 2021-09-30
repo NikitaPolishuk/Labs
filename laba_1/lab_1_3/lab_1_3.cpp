c29a88f866681da702f7fdfba016a669ae6a1b32
NikitaPolishuk <85296053+NikitaPolishuk@users.noreply.github.com>
2021-09-30 14:26:41 +0300

    lab_1

---
+++ laba_1/lab_1_3/lab_1_3.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+char* reverseChar(char* s, int c)
+{
+	int len = 0;
+
+	char* rest_of_string = NULL;
+
+	while (s[len] != '\0')
+	{
+		
+		if (s[len] == c)
+		{
+			rest_of_string = s + len;
+			break; //Выход изи цикла после первого найденного совпадения 
+		}
+		len++;
+	}
+
+	return rest_of_string;
+}
+
+int main()
+{
+	char s[] = "www.some_address.add_something_else.com";
+
+	char* standard_result = strchr(s, '.');
+
+	char* my_result = reverseChar(s, '.');
+
+	printf("Initial string: %s\nStandard: %s\nMy version: %s\n", s, standard_result, my_result);
+
+	return 0;
+
+} 
+
---
+++ laba_1/laba_1/laba_1.cpp
@@ -0,0 +1,66 @@
+﻿
+
+#include <iostream>
+#include <time.h>
+#include "laba_1.h"
+using namespace std;
+
+int processArray(int arrX[], int arrC[], int n, int a, int b)
+{
+	srand(time(0));
+	int numberNegativeNumbers = 0;
+	for (int i = 0; i < n;++i)
+	{
+		int r = rand() % (b-a)+a;
+		if (i % 2) arrX[i] *= r;
+		if (arrX[i] < 0)
+		{
+			arrC[i] = arrX[i];
+			numberNegativeNumbers++;
+		}
+	}
+
+	return numberNegativeNumbers;
+}
+
+void printArray(int arrX[], int arrC[], int n)
+{
+	for (int i = 0; i < n;++i)
+	{
+		printf("%i ",arrX[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < n;++i)
+	{
+		if(arrC[i] !=0 ) printf("%i ", arrC[i]);
+	}
+}
+
+int main()
+{
+	
+	const int n = 15; //размер массива равен 15
+	int x[n] = {};//создание массива
+	int c[n] = {};//создаем выходной массив
+	int a;
+	int b;
+	for (int i = 0; i < n;i++)
+	{
+		x[i] = 1;
+	}
+	cout << "Enter an even number less than 0";
+	cin >> a;
+	cout << "Enter an even number greater than 0";
+	cin >> b;
+	cout << "b =  " << b << endl;
+	cout << "a =  " << a << endl;
+
+	srand(0);//инициализация генератора слуайных чисел
+	int numberNegativeNumbers  = processArray(x,c, n, a, b);
+	printArray(x,c,n);
+	cout << "\nNumber Negative Numbers =  " << numberNegativeNumbers << endl;
+
+
+}
+
+
